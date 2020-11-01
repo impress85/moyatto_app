79392a4bb444e5e9fa7f33aebc8b11994c840dbf
m85hayashi <m85.hayashi.masahiro@gmail.com>
2020-11-01 10:42:34 +0900

    moyatto詳細表示の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "moyattos#index"
-  resources :moyattos, only:[:new,:create]
+  resources :moyattos, only:[:new,:create,:show]
 
 end
