8341e9db043451327587f3e2c92ce4ab83e7df05
m85hayashi <m85.hayashi.masahiro@gmail.com>
2020-11-02 16:22:18 +0900

    仮説の一覧表示と登録機能を実装

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,2 +1,21 @@
 class GuessesController < ApplicationController
+
+  def index
+   @guesses = Guess.all
+  end
+
+  def new
+    @moyatto = Moyatto.find(params[:moyatto_id])
+    @guess = Guess.new
+  end
+
+  def create
+    @guess = Guess.create(guess_params)
+  end
+
+  private
+  def guess_params
+    params.require(:guess).permit(:story).merge(user_id: current_user.id,moyatto_id: params[:moyatto_id])
+  end
+
 end
