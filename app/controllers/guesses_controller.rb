e1c9df480214b35b88cac872dae7722abbbb9f86
m85hayashi <m85.hayashi.masahiro@gmail.com>
2020-11-03 12:50:48 +0900

    仮説削除機能の実装

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -9,28 +9,28 @@ class GuessesController < ApplicationController
     @guess = Guess.create(guess_params)
   end
 
-  # エラーになる Couldn't find Moyatto with 'id'=moyatto_id
   def edit
-    # binding.pry
-   @moyatto = Moyatto.find(params[:moyatto_id])
+    @moyatto = Moyatto.find(params[:moyatto_id])
     @guess = Guess.find(params[:id])
   end
-  # エラーになる Couldn't find Moyatto with 'id'=moyatto_id
+
   def update
     @moyatto = Moyatto.find(params[:moyatto_id])
     # @guesses = @moyatto.guesses.includes(params[:id])
-    # @guess = @guesses.update(guess_params)
     @guess = Guess.find(params[:id])
     @guess.update(guess_params)
   end
 
+  def destroy
+    # moyatto = Moyatto.find(params[:id])
+    guess = Guess.find(params[:id])
+    guess.destroy
+  end
+
   private
     def guess_params
-      params.require(:guess).permit(:story).merge(user_id: current_user.id,moyatto_id: params[:moyatto_id],id: params[:id])
-    end
-
-    def guess_update_params
       params.require(:guess).permit(:story).merge(user_id: current_user.id,moyatto_id: params[:moyatto_id])
+        # ,id: params[:id])
     end
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "moyattos#index"
   resources :moyattos, only:[:new,:create,:show,:edit,:update,:destroy] do
-    resources :guesses, only:[:new,:create,:edit,:update]
+    resources :guesses, only:[:new,:create,:edit,:update,:destroy]
   end
 
 
