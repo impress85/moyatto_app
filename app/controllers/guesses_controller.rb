23f89710ddb58bf6bcabb4e2aa2d4d18b9a8195e
m85hayashi <m85.hayashi.masahiro@gmail.com>
2020-11-11 00:13:45 +0900

    guessバリデーション設定

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,40 +1,53 @@
 class GuessesController < ApplicationController
-
   before_action :authenticate_user! 
-  # before_action :set_moyatto, only: []
+  before_action :authenticate_user! 
+  before_action :set_moyatto, only: [:new,:create,:edit,:update]
+  before_action :set_guess, only: [:edit,:update,:destroy]
   before_action :move_to_index, only: [:edit,:update]
 
   def new
-    @moyatto = Moyatto.find(params[:moyatto_id])
     @guess = Guess.new
   end
 
   def create
-    @guess = Guess.create(guess_params)
+    @guess = Guess.new(guess_params)
+    if @guess.save
     redirect_to moyatto_path(params[:moyatto_id]) 
+    else
+    render :new
+    end
   end
 
   def edit
-    @moyatto = Moyatto.find(params[:moyatto_id])
-    @guess = Guess.find(params[:id])
   end
 
   def update
-    @moyatto = Moyatto.find(params[:moyatto_id])
-    @guess = Guess.find(params[:id])
-    @guess.update(guess_params)
-    redirect_to moyatto_path(params[:moyatto_id]) 
+    if @guess.update(guess_params)
+      redirect_to moyatto_path(params[:moyatto_id]) 
+    else
+      @guess = Guess.find(params[:id])
+      render :edit
+    end
   end
 
   def destroy
-    guess = Guess.find(params[:id])
-    guess.destroy
+    if user_signed_in? && (current_user.id == @guess.user_id)
+    @guess.destroy
     redirect_to moyatto_path(params[:moyatto_id]) 
+    end
   end
 
   private
   def guess_params
     params.require(:guess).permit(:story).merge(user_id: current_user.id,moyatto_id: params[:moyatto_id])
+  end
+
+  def set_moyatto
+    @moyatto = Moyatto.find(params[:moyatto_id])
+  end
+
+  def set_guess
+    @guess = Guess.find(params[:id])
   end
 
   def move_to_index
--- app/models/guess.rb
+++ app/models/guess.rb
@@ -4,4 +4,6 @@ class Guess < ApplicationRecord
   belongs_to :moyatto
   has_one :sukkiri ,dependent: :destroy
 
+  validates :story,presence: true
+
 end
